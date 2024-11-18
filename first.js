2cbe34105502789c74fa410b90ef6daff2ef6c75
24LP8229 24LP8229 <24LP8229@admins-iMac.local>
2024-11-18 12:40:56 +0800

    js first

---
+++ first.js
@@ -0,0 +1,56 @@
+// let birthyear = 2005;
+// let yearnow = 2024;
+// let age = yearnow - birthyear;
+
+// console.log(age);
+// let title = "minii nas";
+// let line1 = "minii nas 19 19 19";
+
+// let title1 = "tanii nas";
+// let line2 = "            tanii nas 24 24 24 24";
+// console.log(title);
+// console.log(line1);
+// console.log(line1);
+// console.log(line1);
+// console.log(title1);
+// console.log(line2);
+// console.log(line2);
+// console.log(line2);
+// console.log(line2);
+
+// let age = 20;
+// if (age > 21) {
+//   console.log("approve");
+// } else {
+//   console.log("Deny");
+// }
+
+// let dun = 50;
+// if (dun > 60) {
+//   if (dun < 101) {
+//     console.log("tentssn");
+//   } else {
+//     console.log("aldaatai");
+//   }
+// } else {
+//   if (dun < 0) {
+//     console.log("aldaatai");
+//   } else {
+//     console.log("unasn");
+//   }
+// }
+
+let drom = true;
+let score = 17;
+if (score > 18) {
+  if (drom == false) console.log("dromndo beld sugaa");
+  else {
+    console.log("tentssn");
+  }
+} else {
+  if (score > 18) {
+    console.log("2ullng beldd");
+  } else {
+    console.log("score oo beld sugaa");
+  }
+}
