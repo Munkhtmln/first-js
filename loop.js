e29ce373ee460ef2adb533f08669699db85c1f14
24LP8229 24LP8229 <24LP8229@admins-iMac.local>
2024-11-20 12:35:40 +0800

    loops

---
+++ loop.js
@@ -0,0 +1,41 @@
+// for (let i = 1; i < 101; i = i + 1) {
+//   console.log(i);
+// }
+// for (let i = 0; i < 101; i = i + 2) {
+//   console.log(i);
+// }
+// for (let i = 99; i > 0; i = i - 3) {
+//   console.log(i);
+// }
+
+// 9
+// let s = 3;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(s, "*", i, "=", s * i);
+// }
+
+// 22
+// let x = 100;
+// for (let i = 1; i <= x; i++) {
+//   if (x % i == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 23
+// let num = 3;
+// let answer = 1;
+// for (i = 1; i <= num; i++) {
+//   answer = answer * i;
+// }
+// console.log(num, "!=", answer);
+
+24;
+let num1 = 18;
+let num2 = 24;
+let min = num1 < num2;
+for (i = 1; i <= min; i++) {
+  if ((num1 % i == 0) & (num2 % i == 0)) {
+  }
+  console.log(min);
+}
