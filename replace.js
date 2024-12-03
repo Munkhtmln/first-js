8deef5728fe7ad3f7efd92d117a25935d9498c67
Munkhtmln <munkhtemuulen0809@gmail.com>
2024-12-03 11:26:53 +0800

    Add files via upload

---
+++ replace.js
@@ -0,0 +1,26 @@
+// let baby = "baby shark"
+// let dodo = "doo doo doo doo doo"
+// for(let i=0;i<5;i++){
+// console.log(baby, dodo, ) 
+// }console.log(baby)
+
+// let daddy = baby.replace("baby", "daddy")
+// for(let i= 0; i<5;i++){
+// console.log(daddy,dodo)
+// } console.log(daddy)
+
+
+// let shark = ["baby shark", "daddy shark", "mommy shark"]
+// let dodo = "ehlel doo doo doo doo doo"
+
+// shark.map((baby) =>{
+//     let line= dodo.replace ("ehlel" , baby)
+//     for(let i= 0 ;i<3;i++){
+//         console.log(line)
+//     } console.log(baby, "!")
+// })
+
+let text = "hello world im fullstack developer"
+
+const mostlength= text.split("fullstack")
+
